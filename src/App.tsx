a40f8d4b23b1d795d538419994ce659bf2906cac
NumanBashir <sportybashir@gmail.com>
2024-02-08 09:27:51 +0100

    fetch API from OpenWeatherMap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
+type WeatherData = {
+  name: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+    pressure: number;
+  };
+  weather: Array<{
+    description: string;
+  }>;
+  wind: {
+    speed: number;
+  };
+};
+
 function App() {
+  const [city, setCity] = useState<string>("London");
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get<WeatherData>(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=167e45ba44d9662755d1a25effac5f34`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [city]);
+
   return (
     <>
-      <h1>Hello World</h1>
-      <p>Hello</p>
+      <div>
+        <h2>{weatherData?.name}</h2>
+        <p>Temperature: {weatherData?.main.temp}°C</p>
+        <p>Description: {weatherData?.weather[0].description}</p>
+        <p>Feels like: {weatherData?.main.feels_like}°C</p>
+        <p>Humidity: {weatherData?.main.humidity}%</p>
+        <p>Pressure: {weatherData?.main.pressure}</p>
+        <p>Wind Speed: {weatherData?.wind.speed}m/s</p>
+      </div>
     </>
   );
 }
